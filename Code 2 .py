41f19a688a61c2926b92a358557808abed0cb751
Gscalone22 <gianas0608@gmail.com>
2025-09-15 10:14:36 -0400

    Code 2 Python

---
+++ Code 2 .py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -
+
+temperature = None
+Response = None
+
+while temperature == None: # Will keep looping as long as the "temp == None" is true.
+    try:
+        temperature = float(input("What is the patients temperature?:" ))
+    except ValueError: # ValueError is when float () is unhappy about it's input.
+        print("Please input a valid value for temperature")
+    
+if temperature >= 99.5:
+    Response = "The patient has a high temperature."
+elif temperature < 99.5:
+    Response = "The patient's temperature is normal."
+    
+print(Response)
